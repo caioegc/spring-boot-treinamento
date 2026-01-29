1a32f8b502bdcf5182bf48abd022851d025b3b9c
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-01-28 23:32:48 -0300

    feat: profiles

--- src/main/java/caio/treinamento/inicio/config/ConnectionConfig.java
+++ src/main/java/caio/treinamento/inicio/config/ConnectionConfig.java
@@ -1,21 +1,32 @@
 package caio.treinamento.inicio.config;
 
 import external.dependency.Conections;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ConnectionConfig {
+    @Value("${database.url}")
+    private String url;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
 
     @Bean
+    @Profile("mysql")
    public Conections conectionsMySql(){
-       return new Conections("caio", "caioegc", "321");
+       return new Conections(url, username  , password);
    }
 
     @Bean
-    @Primary
+    @Profile("mongo")
     public Conections conectionsMongoDB(){
-        return new Conections("caio", "caioegc", "321");
+        return new Conections(url, username, password);
     }
 }
