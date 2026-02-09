a563e85d4fe16ee035c450ee6b93a276b9ca446d
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-02-09 20:41:52 -0300

    feat: testes controller, começando a entender

--- src/test/java/caio/treinamento/inicio/controller/HeroeControllerTest.java
+++ src/test/java/caio/treinamento/inicio/controller/HeroeControllerTest.java
@@ -1,26 +1,24 @@
 package caio.treinamento.inicio.controller;
 
 import caio.treinamento.inicio.entity.Heroe;
-import caio.treinamento.inicio.mapper.HeroeMapperImpl;
 import caio.treinamento.inicio.repository.DataRepository;
 import caio.treinamento.inicio.repository.HeroeRepository;
-import caio.treinamento.inicio.service.HeroeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.servlet.config.MvcNamespaceHandler;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -28,8 +26,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @WebMvcTest(controllers = HeroeController.class)
@@ -50,6 +46,9 @@ class HeroeControllerTest {
 
     @Autowired
     private ResourceLoader resourceLoader;
+
+    @SpyBean
+    private HeroeRepository heroeRepository;
 
     @BeforeEach
     void init() {
@@ -131,7 +130,78 @@ class HeroeControllerTest {
 
     }
 
+    @Test
+    @DisplayName("@DisplayName(POST v1/Heroe salva quando for um sucesso")
+    void salvamentoFeitoQuandoForUmSucesso() throws Exception {
+        var request = readResource("heroe/post-request-heroe-200.json");
+        var response = readResource("heroe/post-response-heroe-201.json");
 
+        var novoHeroeCriado = Heroe.builder().id(99L).nome("Nagato").atDate(LocalDateTime.now()).build();
+        BDDMockito.when(heroeRepository.createHeroe(ArgumentMatchers.any())).thenReturn(novoHeroeCriado);
+
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/v1/heroe")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+
+
+    }
+
+    @Test
+    void atualizeQuandoforUmSucesso() throws Exception {
+        BDDMockito.when(dataRepository.getHEROES()).thenReturn(heroe);
+        var request = readResource("heroe/put-request-heroe-200.json");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/v1/heroe")
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+    }
+
+    @Test
+    void respostaSeNaoExistir() throws Exception {
+        BDDMockito.when(dataRepository.getHEROES()).thenReturn(heroe);
+        var request = readResource("heroe/put-request-heroe-404.json");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/v1/heroe")
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+    }
+
+    @Test
+    void deleteQuandoForUmSucesso() throws Exception {
+        BDDMockito.when(dataRepository.getHEROES()).thenReturn(heroe);
+        var id = heroe.get(0).getId();
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/v1/heroe/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+    }
+
+    @Test
+    void deleteQuandoForUmErro() throws Exception {
+        BDDMockito.when(dataRepository.getHEROES()).thenReturn(heroe);
+        var id = 151561561L;
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/v1/heroe/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+    }
 
     private String readResource(String fileName) throws IOException {
         var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
---
+++ src/test/java/caio/treinamento/inicio/controller/UserControllerTest.java
@@ -0,0 +1,11 @@
+package caio.treinamento.inicio.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class UserControllerTest {
+
+
+}
